a9a4a30ad67d5d845e2c189f1a3e8c126881f3ad
bryanpow <ramosbusiness544@gmail.com>
2024-10-25 13:59:05 -0400

    name input

--- app/referral-form/page.tsx
+++ app/referral-form/page.tsx
@@ -1,36 +1,18 @@
+'use client';
+import { useState, useEffect } from 'react';
+
 export default function page() {
+  
+  const handleSubmit  = async (e: any) => {
+    event?.preventDefault();
+  }
   return (
-    <div className=" bg-gray-100  w-screen h-screen">
-      <div className="flex relative justify-center ">
-        <img
-          className="w-screen rounded absolute  z-[149]  object-[50%_30%]  object-cover h-[13rem]"
-          src="fellows1.png"
-        ></img>
-      </div>
+    <form onSubmit={handleSubmit}>
+        <div >
+            <label htmlFor='name'>Full Name:</label>
+            <input type='text' id='name' name='name' required />
+        </div>
 
-      <div className="w-[60%] mt-[10rem]  relative  border-t-[0.6rem] border-t-[#327a5f] p-5 m-auto z-[150]  bg-white ">
-        <h1 className=" font-medium w-[90%] m-auto  border-b mb-2 text-[2rem]">
-          {" "}
-          MARCY LAB SCHOOL REFERRAL SUBMITION FORM
-        </h1>
-        <p className="w-[90%] m-auto">
-          Help us connect with new talent by submitting a referral. Please
-          provide accurate contact details and any helpful information. LinkedIn
-          profiles and resumes are encouraged. For more details, check the FAQ
-          section.
-        </p>
-      </div>
-
-
-      <div className="w-[60%] mt-10 relative    p-5 m-auto z-[150]  bg-white ">
-        <h1 className=" font-medium w-[90%] m-auto   mb-2 text-[1rem]">
-          {" "}
-          FULL NAME:
-        </h1>
-        <p className="w-[50%] mt-10 border-b ml-[5%]">
-         
-        </p>
-      </div>
-    </div>
+    </form>
   );
 }
