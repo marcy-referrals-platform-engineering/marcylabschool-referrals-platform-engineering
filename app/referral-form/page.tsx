7bf576f8b7a9747957312ffaf11db8a8b0ef1dad
bryanpow <ramosbusiness544@gmail.com>
2024-10-28 12:21:08 -0400

    still working on form

--- app/referral-form/page.tsx
+++ app/referral-form/page.tsx
@@ -29,22 +29,25 @@ export default function page() {
   };
 
   return (
-    <div className="w-screen h-full ">
-    <img src='/fellows1.png' className=" object-cover w-[700px] h-[200px] m-auto"/>
-    <form
-      onSubmit={handleSubmit}
-      className="bg-[#F1F3F4] p-8 max-w-[700px] mx-auto rounded-lg shadow-md"
-    >
-      <h1 className="text-[1.8rem] font-bold mb-4">
-        Marcy Lab School Referral Submission Form
-      </h1>
-      <p className="text-gray-700 mb-6">
-        Help us connect with the next generation of talent. Please use this form
-        to submit a referral. Remember, LinkedIn profiles and resumes are highly
-        encouraged.
-      </p>
+    <div className="w-screen  bg-[#F1F3F4] h-full ">
+      <img
+        src="/fellows1.png"
+        className=" object-cover w-[700px] h-[200px] m-auto"
+      />
+      <form
+        onSubmit={handleSubmit}
+        className=" p-8 bg-white max-w-[700px] mx-auto rounded-lg shadow-md"
+      >
+        <h1 className="text-[1.8rem] font-medium mb-4">
+          MARCY LAB SCHOOL REFERRAL FORM
+        </h1>
+        <p className="text-gray-700 mb-6">
+          Help us connect with the next generation of talent. Please use this
+          form to submit a referral. Remember, LinkedIn profiles and resumes are
+          highly encouraged.
+        </p>
 
-      <div className="bg-white p-6 rounded-lg shadow-sm mb-6">
+        {/* <div className="bg-white p-6 rounded-lg shadow-sm mb-6">
         <h1 className="font-bold text-lg text-gray-800 mb-3">
           Section 1: Referrer Information
         </h1>
@@ -72,104 +75,181 @@ export default function page() {
             ))}
           </div>
         </div>
-      </div>
+      </div> */}
 
-      <div className="bg-white p-6 rounded-lg shadow-sm mb-6">
-        <h1 className="font-bold text-lg text-gray-800 mb-3">
-          Section 2: Candidate Information
-        </h1>
+        <div className="bg-white p-6 rounded-lg shadow-sm mb-6">
+          <h1 className="font-bold text-lg text-gray-800 mb-3">
+            Candidate Information
+          </h1>
 
-        <div className="mb-4">
-          <label
-            htmlFor="recruitName"
-            className="block text-gray-600 font-semibold mb-1"
-          >
-            Full Name:
-          </label>
-          <input
-            type="text"
-            id="recruitName"
-            name="recruitName"
-            value={formData.recruitName}
-            onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded-md"
-            required
-          />
+          <div className="mb-4">
+            <label
+              htmlFor="recruitName"
+              className="block text-gray-600 font-semibold mb-1"
+            >
+              Full Name:
+            </label>
+            <input
+              type="text"
+              id="recruitName"
+              name="recruitName"
+              value={formData.recruitName}
+              onChange={handleChange}
+              className="w-full bg-[#f1f3f463] p-2 border border-gray-300 rounded-md"
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="recruitEmail"
+              className="block text-gray-600 font-semibold mb-1"
+            >
+              Email Address:
+            </label>
+            <input
+              type="email"
+              id="recruitEmail"
+              name="recruitEmail"
+              value={formData.recruitEmail}
+              onChange={handleChange}
+              className="w-full p-2 bg-[#f1f3f463] border border-gray-300 rounded-md"
+              required
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="recruitGender"
+              className="block text-gray-600 font-semibold mb-1"
+            >
+              Gender Identification: (Optional)
+            </label>
+            <p className="text-sm text-gray-500 mb-3">
+              We ask for this information to ensure we're reaching diverse
+              candidates and to track our gender parity goals. Your response is
+              voluntary.
+            </p>
+
+            <div className="space-y-2">
+              <label className="flex items-center space-x-2">
+                <input
+                  type="radio"
+                  name="recruitGender"
+                  value="Female"
+                  checked={formData.recruitGender === "Female"}
+                  onChange={handleChange}
+                  className="form-radio  h-4 w-4"
+                />
+                <span>Female</span>
+              </label>
+
+              <label className="flex items-center space-x-2">
+                <input
+                  type="radio"
+                  name="recruitGender"
+                  value="Male"
+                  checked={formData.recruitGender === "Male"}
+                  onChange={handleChange}
+                  className="form-radio h-4 w-4"
+                />
+                <span>Male</span>
+              </label>
+
+              <label className="flex items-center space-x-2">
+                <input
+                  type="radio"
+                  name="recruitGender"
+                  value="Non-Binary"
+                  checked={formData.recruitGender === "Non-Binary"}
+                  onChange={handleChange}
+                  className="form-radio h-4 w-4"
+                />
+                <span>Non-Binary</span>
+              </label>
+
+              <label className="flex items-center space-x-2">
+                <input
+                  type="radio"
+                  name="recruitGender"
+                  value="Transgender"
+                  checked={formData.recruitGender === "Transgender"}
+                  onChange={handleChange}
+                  className="form-radio h-4 w-4"
+                />
+                <span>Transgender</span>
+              </label>
+
+              <label className="flex items-center space-x-2">
+                <input
+                  type="radio"
+                  name="recruitGender"
+                  value="Prefer Not to Say"
+                  checked={formData.recruitGender === "Prefer Not to Say"}
+                  onChange={handleChange}
+                  className="form-radio h-4 w-4"
+                />
+                <span>Prefer Not to Say</span>
+              </label>
+
+              <label className="flex items-center space-x-2">
+                <input
+                  type="radio"
+                  name="recruitGender"
+                  value="Other"
+                  checked={formData.recruitGender === "Other"}
+                  onChange={handleChange}
+                  className="form-radio h-4 w-4"
+                />
+                <span>Other</span>
+              </label>
+            </div>
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="recruitLinkedIn"
+              className="block text-gray-600 font-semibold mb-1"
+            >
+              LinkedIn Profile (Optional):
+            </label>
+            <p className="text-sm text-gray-500 mb-3">
+            To review candidates background. 
+            </p>
+            <input
+              type="text"
+              id="recruitLinkedIn"
+              name="recruitLinkedIn"
+              value={formData.recruitLinkedIn}
+              onChange={handleChange}
+              className="w-full p-2 border border-gray-300 rounded-md"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-gray-600 font-semibold mb-1">
+              Resume (Optional But Encouraged):
+            </label>
+            <p className="text-sm text-gray-500 mb-3">
+            For quick evaluation. 
+            </p>
+            <input
+              type="file"
+              id="resume"
+              name="resume"
+              onChange={handleChange}
+              className="w-full p-2 border border-gray-300 rounded-md"
+            />
+          </div>
         </div>
 
-        <div className="mb-4">
-          <label
-            htmlFor="recruitEmail"
-            className="block text-gray-600 font-semibold mb-1"
-          >
-            Email Address:
-          </label>
-          <input
-            type="email"
-            id="recruitEmail"
-            name="recruitEmail"
-            value={formData.recruitEmail}
-            onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded-md"
-            required
-          />
-        </div>
-
-        <div className="mb-4">
-          <label
-            htmlFor="recruitGender"
-            className="block text-gray-600 font-semibold mb-1"
-          >
-            Gender Identification (Optional):
-          </label>
-          <input
-            type="text"
-            id="recruitGender"
-            name="recruitGender"
-            value={formData.recruitGender}
-            onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded-md"
-          />
-        </div>
-
-        <div className="mb-4">
-          <label
-            htmlFor="recruitLinkedIn"
-            className="block text-gray-600 font-semibold mb-1"
-          >
-            LinkedIn Profile (Optional):
-          </label>
-          <input
-            type="text"
-            id="recruitLinkedIn"
-            name="recruitLinkedIn"
-            value={formData.recruitLinkedIn}
-            onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded-md"
-          />
-        </div>
-
-        <div className="mb-4">
-          <label className="block text-gray-600 font-semibold mb-1">
-            Resume (Optional But Encouraged):
-          </label>
-          <input
-            type="file"
-            id="resume"
-            name="resume"
-            onChange={handleChange}
-            className="w-full p-2 border border-gray-300 rounded-md"
-          />
-        </div>
-      </div>
-
-      <button
-        type="submit"
-        className="bg-indigo-600 text-white py-2 px-6 rounded-lg hover:bg-indigo-700 transition duration-300"
-      >
-        Submit
-      </button>
-    </form>
+        <button
+          type="submit"
+          className="bg-indigo-600 text-white py-2 px-6 rounded-lg hover:bg-indigo-700 transition duration-300"
+        >
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
