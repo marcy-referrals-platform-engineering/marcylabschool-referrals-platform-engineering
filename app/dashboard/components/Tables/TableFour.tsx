1c9e4d9d1e7a980781604fe61b87019c975c6581
bryanpow <ramosbusiness544@gmail.com>
2024-10-31 14:39:24 -0400

    fixing import

--- app/dashboard/components/Tables/TableFour.tsx
+++ app/dashboard/components/Tables/TableFour.tsx
@@ -1,8 +1,8 @@
-import { BRAND } from "@/types/brand";
+import { BRAND } from "../../types/brand";
 import Image from "next/image";
 import DropdownDefault from "../Dropdowns/DropdownDefault";
 
-const brandData: BRAND[] = [
+const brandData: any[] = [
   {
     logo: "/images/brand/brand-01.svg",
     name: "Google",
