87c5d287213fe71968dfb394faa7a51a4ba860f0
bryanpow <ramosbusiness544@gmail.com>
2024-12-17 15:33:53 -0500

    fixed faq section having old info

--- app/faq/data.ts
+++ app/faq/data.ts
@@ -9,29 +9,20 @@ export const referralFAQ = [
     },
     {
       title: "Who is eligible to be referred?",
-      body: "Anyone who meets our eligibility requirements and demonstrates the following characteristics:\nCuriosity\nDiscipline\nReflective\nHas the Skill & Will to Receive & Implement Feedback\nCollaborative, Values Community\nSolid-to-Strong Organization & Time Management\nStrong Written & Oral Communication Skills\nEnjoys Technology!"
+      body: "Anyone who meets our eligibility requirements and demonstrates the following characteristics:\n-Curiosity\n-Discipline\n-Reflective\n-Has the Skill & Will to Receive & Implement Feedback\nCollaborative, Values Community\n-Solid-to-Strong Organization & Time Management\n-Strong Written & Oral Communication Skills\n-Enjoys Technology!"
     },
     {
       title: "Can I refer someone who has applied before?",
       body: "Yes, but only if they have not been in contact with our Recruitment/Admissions (R/A) team during the last two admission cycles and have not submitted a new application within that time. Contact includes meetings (virtual or in-person), significant email correspondence, or event attendance."
     },
  
-    {
-      title: "When will the person I referred receive communication from the admissions team?",
-      body: "The admissions team will reach out within two business days of the referral submission."
-    },
-    {
-      title: "When will I know my total points?",
-      body: "At the end of each admission round, please see the admission calendar here. We will email you to notify you of the total number of points you will receive."
-    },
+   
+   
     {
       title: "What is the maximum number of points I can receive for one referral?",
       body: "380 points"
     },
-    {
-      title: "How many people can I refer?",
-      body: "You can refer as many people as you desire. However, you can only receive points for up to three prospects at each admission round. If you have made referrals for five candidates, and each has successfully reached every milestone, the maximum points you are eligible to receive would amount to 1,140. This calculation is based on three referrals, from the initial referral submission to the point at which the candidate attains the status of a Marcy Fellow."
-    },
+  
     {
       title: "How are rewards distributed?",
       body: "After calculating the total number of points at the end of the admission cycle, a member of the R/A team will contact you to choose a reward."
